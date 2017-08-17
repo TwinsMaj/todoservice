ad457c840513dc33cfc3eb03da88599bce05d69d
Ridwan Maja <majataiwo@outlook.com>
2017-08-17 16:25:07 +0100

    finish improvements on todo-router

--- api/v1/todos/todo-controller.js
+++ api/v1/todos/todo-controller.js
@@ -1,5 +1,17 @@
 var todoModel = require('./todo-model.js');
 
+exports.intercept = function(req, res, next, id) {
+	todoModel.findById(id, function(err, todo) {
+		if(err) {
+			req.errstatus = 404;
+			return next(new Error("Invalid ID. cannot find todo"));
+		}
+
+		req.todo = todo
+		next();
+	})
+}
+
 exports.addTodo = function(req, res, next) {
 	// prepare a new todo model with incoming req
 	var todo = new todoModel(req.body);
@@ -27,13 +39,5 @@ exports.fetchAllTodos = function(req, res, next) {
 
 exports.fetchTodo = function(req, res, next) {
 	var id = req.params.id
-
-	todoModel.findById(id, function(err, todo) {
-		if(err) {
-			req.errstatus = 404;
-			return next(new Error("Invalid ID. cannot find todo"));
-		}
-
-		res.status(200).json(todo);
-	})
+	res.status(200).json(req.todo);
 }
--- api/v1/todos/todo-router.js
+++ api/v1/todos/todo-router.js
@@ -1,10 +1,14 @@
 var express 		= require('express'),
-	todoRouter      = express(),
+	todoRouter      = express.Router(),
 	todoController  = require('./todo-controller.js');
 
+todoRouter.param("id", todoController.intercept)
 
-todoRouter.post('/', todoController.addTodo)
-todoRouter.get('/', todoController.fetchAllTodos)
-todoRouter.get('/:id', todoController.fetchTodo)
+todoRouter.route('/')
+	.post(todoController.addTodo)
+	.get(todoController.fetchAllTodos)
+
+todoRouter.route('/:id')
+	.get(todoController.fetchTodo)
 
 module.exports = todoRouter;

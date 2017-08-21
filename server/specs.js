6048fbf437b7a7593c7d51a09e653b901a53ce33
Ridwan Maja <majataiwo@outlook.com>
2017-08-21 11:11:44 +0100

    todo post route test passing

--- server/specs.js
+++ server/specs.js
@@ -5,27 +5,30 @@ var request = require('supertest'),
 	expect  = chai.expect,
 	should  = chai.should();
 
-describe("#todo", function() {
+describe('#todosRoutesSpecs', function() {
 
-	it("should insert a todo item", function(done) {
-		var todo = {
-			task: "do some  testing",
-			date: "17th Aug, 2017",
-			time: "5pm"
+	it("should add a todo item to the db", function(done) {
+		var data = {
+			task: "do some tests",
+			date: "20th Aug, 2017",
+			time: "11:00pm"
 		}
 
 		request(app)
 			.post('/api/v1/todos')
-			.send(todo)
+			.set("Content-Type", "Application/json")
+			.send(data)
 			.end(function(err, res) {
-
 				expect(res.body).to.be.an('object');
-				assert(res.body.time, "5pm")
-
+				expect(res.body.task).to.be.equal("do some tests");
+				expect(res.body.date).to.be.equal("20th Aug, 2017");
+				expect(res.body.time).to.be.equal("11:00pm");
 				done();
 			})
 	})
 
+	/*xit("should get a tod item from the db", function(done) {
 
-	it.skip("should return alltod")
+	})*/
+
 })
